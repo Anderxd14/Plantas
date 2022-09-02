3952af9ff3267d35b40466a2f9519a26e265d197
Anderson Garnica <steven14.as@gmail.com>
2022-09-02 15:13:18 -0500

    inicio frontend con react

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Layout = ({ chidlren }) => {
+    return(
+        <div className = "Layout">
+            {chidlren}
+        </div>
+    );
+}
+
+export default Layout;
