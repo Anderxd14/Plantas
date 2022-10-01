42ecf43ae53dec8a63afac55372f7940461104f2
Anderson Garnica <steven14.as@gmail.com>
2022-10-01 14:52:10 -0500

    se corrige el error de brayan xd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,16 +17,25 @@ const Header = () => {
 
 
             <div class="slides">
-                
+
                 <div className="slide">
-                    <img src="https://i.pinimg.com/564x/67/a5/30/67a5308ca8e67dea84d833728276c84d.jpg" alt="" class="slide-image" /> 
+                    <img src="https://i.pinimg.com/564x/67/a5/30/67a5308ca8e67dea84d833728276c84d.jpg" alt="" class="slide-image" />
                 </div>
-    
+                <div className="slide slide-2">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuZlmOk3Wnfv1Dkmrg9YJ2qRfddD6sA7OTh-OYJxT6qrbcy360H5CctTHYJw0k-KEFnB8&usqp=CAU.jpg" alt="" className="slide-image" />
+                </div>
+                <div className="slide slide-3">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQywUGe9gN649b2vKngdujyoamn37JwdB5sAg&usqp=CAU.jpg" alt="" className="slide-image" />
+                </div>
+                <div className="slide slide-4">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR76Npy9L4l2vLWjD7qG6_5MBvTA7ZjAsqAQ&usqp=CAU.jpg" alt="" className="slide-image" />
+                </div>
+
             </div>
 
             <div className="slidesp">
-            <p>Este Es un Pequeño Proyecto universitario, para promover la automatizacion para el control de humedad y riego, 
-            de manera facil y accesible para todos los pequeños agricultores de colombia.</p>   
+                <p>Este Es un Pequeño Proyecto universitario, para promover la automatizacion para el control de humedad y riego,
+                    de manera facil y accesible para todos los pequeños agricultores de colombia.</p>
             </div>
 
         </div>
