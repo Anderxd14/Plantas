9007059f410b3a9be111fed025ad92b4a7230f7e
Anderson Garnica <steven14.as@gmail.com>
2022-10-01 14:04:52 -0500

    centrado parrafo clase slidesp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,13 +21,13 @@ const Header = () => {
                 <div className="slide">
                     <img src="https://i.pinimg.com/564x/67/a5/30/67a5308ca8e67dea84d833728276c84d.jpg" alt="" class="slide-image" /> 
                 </div>
-                
-                
+    
             </div>
 
-             <p>Este Es un Pequeño Proyecto universitario, para promover la automatizacion para el control de humedad y riego, 
-                de manera facil y accesible para todos los pequeños agricultores de colombia.</p>   
-
+            <div className="slidesp">
+            <p>Este Es un Pequeño Proyecto universitario, para promover la automatizacion para el control de humedad y riego, 
+            de manera facil y accesible para todos los pequeños agricultores de colombia.</p>   
+            </div>
 
         </div>
 
