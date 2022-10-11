ec7eeefe842ef9f517abcd753e81ac9aa811719f
Brayan <juliherrera237@gmail.com>
2022-10-10 21:46:32 -0500

    Cambio en perfil

--- src/components/PerfilC.jsx
+++ src/components/PerfilC.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import "../styles/perfil.scss"
+import "../styles/busqueda.scss"
+import "../styles/muestra.scss"
+
 
 const Perfil = () => {
     return (
-        <div className="PerfilC">
+        <div className="Perfil">
             <div className="Dashboard-der">
-                menu deracha
-            </div>
-                
-            <div className="Dashboard-iz">
-                menu izquierda
+                menu derecha
+                <form class="busqueda">
+                    <input type="text" placeholder="Buscar..." class="input" />
+                    <button type="submit" class="button">
+                    </button>
+                </form>
+
+
+                <div className="container">
+                    <article class="card">
+                        <h4 class="card_title" >Instagram </h4>
+                    </article>
+                    <div class="card">
+                        <h4 class="card_title" >Facebook </h4>
+                    </div>
+                    <article class="card">
+                        <h4 class="card_title" >Yolo </h4>
+                    </article>
+                </div>
+                <div className="Dashboard-iz">
+                    menu izquierda
+                </div>
             </div>
         </div>
     );
